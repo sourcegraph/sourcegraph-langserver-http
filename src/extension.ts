2c09fb37872de7c2620e990059533f8c4672a321
Quinn Slack <qslack@qslack.com>
2018-11-29 22:50:39 -0800

    feat: support deactivation
    See https://docs.sourcegraph.com/extensions/authoring/activation.

--- src/extension.ts
+++ src/extension.ts
@@ -2,39 +2,56 @@ import * as sourcegraph from 'sourcegraph'
 import { toHover } from './backcompat'
 import { sendLSPRequest } from './lsp'
 
+// No-op for Sourcegraph versions prior to 3.0-preview. See
+// https://docs.sourcegraph.com/extensions/authoring/activation#backcompat-for-sourcegraph-versions-prior-to-3-0-preview.
+const DUMMY_CTX = { subscriptions: { add: (_unsubscribable: any) => void 0 } }
+
 /** Entrypoint for the language server HTTP adapter Sourcegraph extension. */
-export function activate(): void {
-    activateWith({ provideLSPResults })
+export function activate(ctx: sourcegraph.ExtensionContext = DUMMY_CTX): void {
+    activateWith(ctx, { provideLSPResults })
 }
 
-export function activateWith({
-    provideLSPResults,
-}: {
-    provideLSPResults: (
-        method: string,
-        doc: sourcegraph.TextDocument,
-        pos: sourcegraph.Position,
-        initializationOptions?: any
-    ) => Promise<any>
-}): void {
-    sourcegraph.languages.registerHoverProvider(['*'], {
-        provideHover: async (doc, pos) => {
-            const result = await provideLSPResults('textDocument/hover', doc, pos)
-            return toHover(result) // backcompat
-        },
-    })
-    sourcegraph.languages.registerDefinitionProvider(['*'], {
-        provideDefinition: (doc, pos) => provideLSPResults('textDocument/definition', doc, pos),
-    })
-    sourcegraph.languages.registerTypeDefinitionProvider(['*'], {
-        provideTypeDefinition: (doc, pos) => provideLSPResults('textDocument/typeDefinition', doc, pos),
-    })
-    sourcegraph.languages.registerImplementationProvider(['*'], {
-        provideImplementation: (doc, pos) => provideLSPResults('textDocument/implementation', doc, pos),
-    })
-    sourcegraph.languages.registerReferenceProvider(['*'], {
-        provideReferences: (doc, pos) => provideLSPResults('textDocument/references', doc, pos),
-    })
+export function activateWith(
+    ctx: sourcegraph.ExtensionContext,
+    {
+        provideLSPResults,
+    }: {
+        provideLSPResults: (
+            method: string,
+            doc: sourcegraph.TextDocument,
+            pos: sourcegraph.Position,
+            initializationOptions?: any
+        ) => Promise<any>
+    }
+): void {
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerHoverProvider(['*'], {
+            provideHover: async (doc, pos) => {
+                const result = await provideLSPResults('textDocument/hover', doc, pos)
+                return toHover(result) // backcompat
+            },
+        })
+    )
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerDefinitionProvider(['*'], {
+            provideDefinition: (doc, pos) => provideLSPResults('textDocument/definition', doc, pos),
+        })
+    )
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerTypeDefinitionProvider(['*'], {
+            provideTypeDefinition: (doc, pos) => provideLSPResults('textDocument/typeDefinition', doc, pos),
+        })
+    )
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerImplementationProvider(['*'], {
+            provideImplementation: (doc, pos) => provideLSPResults('textDocument/implementation', doc, pos),
+        })
+    )
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerReferenceProvider(['*'], {
+            provideReferences: (doc, pos) => provideLSPResults('textDocument/references', doc, pos),
+        })
+    )
 }
 
 export async function provideLSPResults(
